5e9fe0b50394a53ac5aabc4cfd5e8a62f8396e23
FOAna <kristendamon@yandex.ru>
2021-08-05 23:55:32 +0500

    add database

--- app.js
+++ app.js
@@ -1,23 +1,86 @@
-const express = require('express'); // загружаем библиотеку
+const express = require("express"); // require - загружаем библиотеку
+const pg = require("pg");
+const Sequelize = require("sequelize");
 const app = express(); // создаём приложение
 const port = 3000; // порт, на котором будем слушать (сервер слушает, а клиент обращается)
 
-app.set('view engine', 'hbs'); // указываем используемый шаблонизатор, view engine - механизм визуализации
+const conString =
+  "postgres://jtkfeqxe:nSjEsod-wyuApkKDtP8zqrGVSyPqrslc@hattie.db.elephantsql.com/jtkfeqxe"; // сохранение адреса базы данных
+const client = new pg.Client(conString); // создаём клиент, который может ходить в базу данных
 
-app.use(express.static('public')); // express раздаёт static из папки public
+// конфигурация Sequelize, который будет ходить в базу данных
+const sequelize = new Sequelize(
+  // вся информация берётся из адреса базы данных postgres://username:password@hostname/databasename
+  "jtkfeqxe", // имя базы данных
+  "jtkfeqxe", // пользователь
+  "nSjEsod-wyuApkKDtP8zqrGVSyPqrslc", // пароль
+  {
+    dialect: "postgres",
+    host: "hattie.db.elephantsql.com",
+  }
+);
 
-// по пути / отдавай то, что в res.send; request - запрос клиента, response - ответ сервера
-app.get('/', (req, res) => {
-  //res.send('Hello World!');
-  //res.sendFile("index.hbs", { root: '.' }); // root - это путь к файлу
-  res.render('index', { static: `http://localhost:${port}`}); //
-});
+// Madel - это информация об устройстве таблицы
+class Tasks extends Sequelize.Model {} // создаём модель таблицы Tasks, которая наследует все методы из Sequelize.Model
+Tasks.init(
+  // описание столбцов таблицы
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+    },
+    content: {
+      type: Sequelize.STRING,
+    },
+    status: {
+      type: Sequelize.STRING,
+    },
+  },
+  { sequelize, modelName: "task" } // sequelize то же самое, что sequelize: sequelize
+);
 
-app.get('/bla', (req, res) => {
-    res.send("Как неожиданно и приятнааа!");
-})
+// синхронизация с базой данных, возвращает Promise
+sequelize.sync({ force: true }).then(() => {
+  app.set("view engine", "hbs"); // указываем используемый шаблонизатор, view engine - механизм визуализации
 
-// "Слушай на порту 3000 ..."
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  app.use(express.static("public")); // express раздаёт static из папки public
+
+  // по пути / отдавай то, что в res.send; request - запрос клиента, response - ответ сервера
+  app.get("/", (req, res) => {
+    //res.send('Hello World!');
+    //res.sendFile("index.hbs", { root: '.' }); // root - это путь к файлу
+    res.render("index", { static: `http://localhost:${port}` }); //
+  });
+
+  app.get("/bla", (req, res) => {
+    sequelize
+      .authenticate() // возвращает Promise
+      .then(() => {
+        Tasks.create({ content: "Пробная задача" }); // создание задачи, строки в таблице Tasks
+      })
+      .catch((err) => res.send("Connection error: ", err))
+      .then(res.send("Завершили работу с базой данных"));
+    /* // после установки соединения (connect) вызывает переданную функцию, передаёт в эту функцию результат установки соединения
+  // // потом закроет соединение
+  // client.connect(function (err) {
+  //   if (err) {
+  //     return console.error("could not connect to postgres", err); // ранний return вместо else
+  //   }
+  //   // делает SQL-запрос, переданный первым аргументом, потом вызывает функцию, переданную вторым аргументом,
+  //   // и передаёт в неё результат выполнения SQL-запроса
+  //   client.query("SELECT * FROM testTable", function (err, result) {
+  //     if (err) {
+  //       return console.error("error running query", err);
+  //     }
+  //     res.send(result); // отправление результата на клиент (например, вкладка браузера)
+  //     // >> output: 2018-08-23T14:02:57.117Z
+  //     client.end();
+  //   });
+     });*/
+  });
+
+  // "Слушай на порту 3000 ..."
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
 });
